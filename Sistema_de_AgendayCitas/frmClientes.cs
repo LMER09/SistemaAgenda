6a4b64c9859b8c8aa83a157a0cbfdb9ece7a87b6
LUZMAIRY ESPIRITUSANTO RODRIGUEZ <le2025-0558@uce.edu.do>
2026-07-02 11:42:31 -0400

    :[

--- Sistema_de_AgendayCitas/frmClientes.cs
+++ Sistema_de_AgendayCitas/frmClientes.cs
@@ -110,5 +110,9 @@ namespace SistemaAgenda.UI
                 e.Handled = true;
         }
 
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
     }
 }
